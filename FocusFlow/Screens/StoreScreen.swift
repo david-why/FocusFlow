42dfbcdf59a1da59327c65b9c3c2e9d6a57c78bc
david-why <david_why@outlook.com>
2025-10-06 10:00:42 +0800

    Add app icons in store

--- FocusFlow/Screens/StoreScreen.swift
+++ FocusFlow/Screens/StoreScreen.swift
@@ -12,11 +12,24 @@ struct StoreItem: Identifiable {
     let name: LocalizedStringKey
     let description: LocalizedStringKey
     let price: Int
+    var image: String? = nil
+    var special: StoreItemSpecial = .none
+    var single = false
+    
+    enum StoreItemSpecial {
+        case none
+        case appIcon(name: String)
+    }
 }
 
 let storeItems = [
     StoreItem(id: "break-1", name: "1-minute Break Pass", description: "Use this pass to take a 1 minute break on your phone in a focus session! Automatically applied when you leave the app.", price: 30),
-    StoreItem(id: "break-5", name: "5-minute Break Pass", description: "Use this pass to take a 5 minute break on your phone in a focus session! Automatically applied when you leave the app.", price: 200)
+    StoreItem(id: "break-5", name: "5-minute Break Pass", description: "Use this pass to take a 5 minute break on your phone in a focus session! Automatically applied when you leave the app.", price: 200),
+    StoreItem(id: "icon-rainbow", name: "Rainbow App Icon", description: "Unlock the Rainbow app icon, joyful and diverse like a burst of color!", price: 60, image: "icon_rainbow", special: .appIcon(name: "AppIconRainbow"), single: true),
+    StoreItem(id: "icon-coral", name: "Coral App Icon", description: "Unlock the Coral app icon, vibrant and warm like an ocean sunset!", price: 60, image: "icon_coral", special: .appIcon(name: "AppIconCoral"), single: true),
+    StoreItem(id: "icon-frost", name: "Frost App Icon", description: "Unlock the Frost app icon, cool and crisp like a winter morning!", price: 60, image: "icon_frost", special: .appIcon(name: "AppIconFrost"), single: true),
+    StoreItem(id: "icon-violet", name: "Violet App Icon", description: "Unlock the Violet app icon, mysterious and regal like a twilight sky!", price: 60, image: "icon_violet", special: .appIcon(name: "AppIconViolet"), single: true),
+    StoreItem(id: "icon-emerald", name: "Emerald App Icon", description: "Unlock the Emerald app icon, fresh and lively like a lush forest!", price: 60, image: "icon_emerald", special: .appIcon(name: "AppIconEmerald"), single: true)
 ]
 
 struct StoreScreen: View {
@@ -27,6 +40,9 @@ struct StoreScreen: View {
             List {
                 Section {
                     Text("You have: \(coinText) \(coins)")
+                    Button("Reset app icon") {
+                        UIApplication.shared.setAlternateIconName(nil)
+                    }
                 }
                 
                 ForEach(storeItems) { item in
@@ -67,6 +83,14 @@ struct StoreItemView: View {
     
     var body: some View {
         VStack {
+            if let image = item.image {
+                Image(image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth: 150)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    .padding(.bottom, 8)
+            }
             Text(item.name)
                 .font(.headline)
             Text("\(coinText) \(item.price)")
@@ -90,14 +114,19 @@ struct PurchaseItemView: View {
     @State var quantity = 1
     
     var body: some View {
-        Stepper(value: $quantity, in: 1...Int.max) {
-            Text("Quantity: \(quantity)")
+        if !item.single {
+            Stepper(value: $quantity, in: 1...Int.max) {
+                Text("Quantity: \(quantity)")
+            }
         }
         VStack(alignment: .leading) {
             Text("You need: \(coinText) \(coinsNeeded)")
             Text("You have: \(coinText) \(coinsHeld)")
             if !hasEnoughCoins {
                 Text("Not enough coins!")
+                    .foregroundStyle(.red)
+            } else if hasReachedLimit {
+                Text("Reached purchase limit")
                     .foregroundStyle(.red)
             } else {
                 Text("Coins remaining: \(coinText) \(coinsHeld - coinsNeeded)")
@@ -106,7 +135,11 @@ struct PurchaseItemView: View {
         Button("Purchase!") {
             performPurchase(quantity: quantity)
         }
-        .disabled(!hasEnoughCoins)
+        .disabled(!canPurchase)
+        if case .appIcon = item.special {
+            Button("Use this icon!", action: useAppIcon)
+                .disabled(!hasReachedLimit)
+        }
     }
     
     var coinsNeeded: Int {
@@ -115,6 +148,14 @@ struct PurchaseItemView: View {
     
     var hasEnoughCoins: Bool {
         coinsNeeded <= coinsHeld
+    }
+    
+    var hasReachedLimit: Bool {
+        item.single && storeService.count(of: item.id) != 0
+    }
+    
+    var canPurchase: Bool {
+        hasEnoughCoins && !hasReachedLimit
     }
     
     // MARK: - Actions
@@ -126,9 +167,17 @@ struct PurchaseItemView: View {
         }
         coinsHeld -= item.price * quantity
     }
+    
+    func useAppIcon() {
+        guard case let .appIcon(name) = item.special else { return }
+        UIApplication.shared.setAlternateIconName(name)
+    }
 }
 
 #Preview {
     StoreScreen()
         .environment(StoreService())
+        .onAppear {
+            UserDefaults.standard.set(5000, forKey: "coins")
+        }
 }
